5bd462c04857d5a5567c19411c77d50faeffd604
Harrison <harrisontran@outlook.com>
2021-06-28 21:46:58 -0700

    add prebuild and run build file

---
+++ build/precache-manifest.44e92cbbf5aae92a7270148d357d97d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1081668cbe8206d968047eeb460356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5e5df0b230b4f4bce4",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5e5df0b230b4f4bce4",
+    "url": "/static/js/main.1e8ea1dc.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
